00baf3f145d76a63622c521ef9867cbf6305fec8
Serabass <is_everything@mail.ru>
2018-01-28 04:07:36 +0600

    upd

--- bin/cli.js
+++ bin/cli.js
@@ -1,41 +1,52 @@
 var argv = require('minimist')(process.argv.slice(2));
+var path = require('path');
+var fs = require('fs');
 
-// TODO Add a tty support
 // For example: cat file.html | parser ".selector" :fn
 
 var source, selector, processing;
 
 if (argv._.length === 3) {
+    if (!process.stdin.isTTY)
+        throw new Error('Three arguments may only passed in tty mode (non pipe)');
+
+    source = process.stdin;
+
     [source, selector, processing] = argv._;
 } else if (argv._.length === 2) {
+
+    if (process.stdin.isTTY)
+        throw new Error('Two arguments may only passed in non-tty mode (pipe)');
+
+    source = process.stdin;
+
     [selector, processing] = argv._;
+} else if (argv._.length === 0) {
+
+    if (argv['init-config'] === true) {
+        var src = path.join(__dirname, '../.parserconfig.js');
+        var dest = path.join(process.cwd(), '.parserconfig.js');
+        fs.copyFileSync(src, dest);
+        console.log('Config file copied successfully');
+        return;
+    }
+
+    showHelp();
+    return;
 } else {
     throw new Error(`Wrong arguments count: ${argv._.length}`);
 }
 
 var Parser = require('../lib/index');
-var fs = require('fs');
-var cwd = process.cwd();
-var path = require('path');
-var extendedConfigPath = path.join(cwd, '.parserconfig.js');
-
 (async () => {
-    var parser;
-    if (process.stdin.isTTY) {
-        // non piped
-        parser = new Parser(source);
-    } else {
-        // piped
-        parser = new Parser(process.stdin);
-    }
-
-    if (fs.existsSync(extendedConfigPath)) {
-        var config = require(extendedConfigPath);
-        parser.extend(config);
-    }
-
-    var $ = await parser.load();
-    let proc = parser.getProcessor(processing);
-    let data = await Promise.resolve(proc.processor.call($(selector), ...proc.args));
-    console.log(data);
+    let result = await Parser.process(source, selector, processing);
+    console.log(result);
 })();
+
+function showHelp() {
+    console.log('Usage:');
+    console.log('   pageparser http://example.com/ "<selector>" :<function>');
+    console.log('   cat <file> | pageparser "<selector>" :<function>');
+    console.log('   pageparser "<selector>" :<function> < <file>');
+    console.log('   pageparser --init-config - Place example .parserconfig.js to cwd');
+}
--- sandbox.js
+++ sandbox.js
@@ -1,10 +1,6 @@
 var Parser = require('./lib/index');
 
 (async () => {
-  var parser = new Parser({
-    url: 'http://example.com/'
-  });
-  var $ = await parser.load('http://example.com/');
-  var s = $('h1').html();
-  debugger;
+    var result = await Parser.process('tests/testpage.html', '.we > .need > .to > .go > .deeper', ':html');
+    console.log(result);
 })();
