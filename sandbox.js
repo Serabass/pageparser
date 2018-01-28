23f8663779f230a44989c4e4b78fb80e1c40366c
Serabass <is_everything@mail.ru>
2018-01-28 17:23:21 +0600

    removed sandbox.js

--- sandbox.js
+++
@@ -1,6 +0,0 @@
-var Parser = require('./lib/index');
-
-(async () => {
-    var result = await Parser.process('tests/testpage.html', '.we > .need > .to > .go > .deeper', ':html');
-    console.log(result);
-})();
